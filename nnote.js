04572155374ff8bfdf758301c2d108b84b333085
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-22 16:13:18 -0700

    add

--- nnote.js
+++ nnote.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const readline = require('readline');
 
-var readline = require('readline');
-
+const DEBUGGING = false
+function dtrace(message) {
+  if (DEBUGGING) {
+    console.log(message)
+  }
+}
 async function getPage() {
-
-
   browser = await puppeteer.launch({
     headless: false,
     timeout: 30000 * 10,
@@ -15,44 +17,72 @@ async function getPage() {
     ]
   })
 
-  page = true
   page = await browser.newPage();
-  await page.setContent('<pre></pre>')
+  await page.setContent(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre></pre>
+</body>
+</html>
+  `)
 
-
-  return page
+  return [browser, page]
 }
 
-var bOn=false
 async function main() {
   getPage()
-    .then(page => {
+    .then(async ([browser, page]) => {
+
       var rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
         terminal: false
       });
 
-      if(!bOn) {
-        bOn=true
-      rl.on('line',aaa )
-      }
+      // rl.on('line', async (line) => {
+      //   let preElement = await page.$('pre')
 
-      async function aaa(line) {
-        // console.log(line);
-        // let page = await getPage()
-        let preElement = await page.$('pre')
-        let all = await page.evaluate(pre => pre.innerText, preElement)
-
-        all += line
-        all += "\n";
-
-        await page.evaluate((pre, all) => {
-          pre.innerText = all
-        }, preElement, all);
-      }
+      //   await page.evaluate((pre, line) => {
+      //     pre.innerText += (line + "\n")
+      //   }, preElement, line);
+      // })
+      let waits = []
+      rl.on('line', (line) => {
+        let prom = page.$('pre')
+          .then((preElement) => {
+            return page.evaluate((pre, line) => {
+              pre.innerText += (line + "\n")
+            }, preElement, line);
+          })
+          .catch(err => console.log(err))
+        waits.push(prom)
+      })
+      rl.on('close', () => {
+        dtrace('stdin closed')
+        Promise.all(waits)
+          .then((waits) => {
+            dtrace(waits)
+            browser.disconnect()
+            dtrace('disconnected')
+            // Looks like we can not exit without closing chromium
+            // browser.close()
+            //   .then(() => {
+            //     dtrace('closed')
+            //     // process.exit(0)
+            //   })
+            //   .catch(err => console.log(err))
+          })
+          .catch(err => console.log(err))
+      })
     })
-
+    .catch(err => console.log(err))
 
 }
 
